c49744b81232d8b7a87648b31e9926beaf73b7bf
calmdawn <qnfqus@gmail.com>
2022-05-15 23:58:57 +0900

    gitignore 추가

---
+++ 코테준비/src/백준/No3_10171.java
@@ -0,0 +1,10 @@
+package 백준;
+
+public class No3_10171 {
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
