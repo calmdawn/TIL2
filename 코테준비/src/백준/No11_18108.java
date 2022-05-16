f3dac236835bbd0dd45f13910b1e321b9e1974f7
calmdawn <qnfqus@gmail.com>
2022-05-16 18:51:24 +0900

    no9 ~ no13 complete, 입출력과 사칙연산 완료

---
+++ 코테준비/src/백준/No11_18108.java
@@ -0,0 +1,13 @@
+package 백준;
+
+import java.util.Scanner;
+
+public class No11_18108 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int year = scan.nextInt();
+        scan.close();
+
+        System.out.println(year - 543);
+    }
+}
