a412a52d9ee96c9983ce70d9a69b889ec4af3fbc
calmdawn <qnfqus@gmail.com>
2022-05-17 02:18:23 +0900

    백준 조건문 complete no4 ~ no7 complete

---
+++ 코테준비/src/백준/조건문/No6_2525.java
@@ -0,0 +1,25 @@
+package 백준.조건문;
+
+import java.util.Scanner;
+
+public class No6_2525 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt(); // 현재 시
+        int B = scan.nextInt(); // 현재 분
+        int C = scan.nextInt(); // 요리하는데 필요한 시간
+        int h, m;
+
+        if (B + C >= 60) {
+            h = A + ((B + C) / 60);
+            m = (B + C) % 60;
+            if (h > 23)
+                h = h - 24;
+        } else {
+            h = A;
+            m = B + C;
+        }
+
+        System.out.println(h + " " + m);
+    }
+}
