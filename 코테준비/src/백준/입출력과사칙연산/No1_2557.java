43941c860f038de2611cb7b202e1bdc6f28d73f6
calmdawn <qnfqus@gmail.com>
2022-05-16 23:36:46 +0900

    단계별 구조 구별, 조건문 풀이 조건문 no1 ~ no3 완료

--- 코테준비/src/백준/입출력과사칙연산/No1_2557.java
+++ 코테준비/src/백준/입출력과사칙연산/No1_2557.java
@@ -1,4 +1,4 @@
-package 백준;
+package 백준.입출력과사칙연산;
 
 public class No1_2557 {
     public static void main(String[] args){
--- 코테준비/src/백준/입출력과사칙연산/No7_10998.java
+++ 코테준비/src/백준/입출력과사칙연산/No7_10998.java
@@ -1,13 +1,13 @@
-package 백준;
+package 백준.입출력과사칙연산;
 
 import java.util.Scanner;
 
-public class No6_1001 {
+public class No7_10998 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
 
-        System.out.print(a - b);
+        System.out.print(a * b);
     }
 }
