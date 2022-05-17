cfe15e8efb1be79f3a87089d6ff4c93fecc02805
calmdawn <qnfqus@gmail.com>
2022-05-17 23:30:45 +0900

    반복문 no6 ~ no9 complete

---
+++ 코테준비/src/백준/반복문/No9_2438.java
@@ -0,0 +1,20 @@
+package 백준.반복문;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class No9_2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            for(int j = 0; j <= i; j++)
+                bw.write("*");
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
