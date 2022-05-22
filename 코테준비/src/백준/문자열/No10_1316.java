a018b9cc9ca127d3f3eecd7d12d68a998545e1fd
calmdawn <qnfqus@gmail.com>
2022-05-22 23:05:38 +0900

    No6 ~ No10 complete, 문자열 완료

---
+++ 코테준비/src/백준/문자열/No10_1316.java
@@ -0,0 +1,45 @@
+package 백준.문자열;
+
+import java.io.*;
+
+public class No10_1316 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (checkGroupWord(br.readLine()))
+                count++;
+        }
+
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
+    }
+
+    private static boolean checkGroupWord(String word) {
+        boolean[] alpha = new boolean[26];
+
+        char prev = word.charAt(0);
+        char next = 'a';
+
+        for (int i = 1; i < word.length(); i++) {
+            next = word.charAt(i);
+            if (alpha[prev - 'a'] == true)
+                return false;
+            if (prev == next)
+                prev = next;
+            else {
+                alpha[prev - 'a'] = true;
+                prev = next;
+            }
+        }
+        if (alpha[next - 'a'] == true)
+            return false;
+
+        return true;
+    }
+}
---
+++ 코테준비/src/백준/문자열/No9_2941.java
@@ -0,0 +1,52 @@
+package 백준.문자열;
+
+import java.io.*;
+
+public class No9_2941 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String word = br.readLine();
+
+        bw.write(String.valueOf(checkAlpha(word)));
+        bw.flush();
+        bw.close();
+    }
+
+    public static int checkAlpha(String word) {
+        int idx = 0;
+        int count = 0;
+        while (idx < word.length()) {
+            if (word.indexOf("c=", idx) == idx) { //현재위치인 idx 위치부터 탐색시작 위치가 같은경우 해당 단어찾음
+                count++;
+                idx = idx + 2;
+            } else if (word.indexOf("c-", idx) == idx) {
+                count++;
+                idx = idx + 2;
+            } else if (word.indexOf("dz=", idx) == idx) {
+                count++;
+                idx = idx + 3;
+            } else if (word.indexOf("d-", idx) == idx) {
+                count++;
+                idx = idx + 2;
+            } else if (word.indexOf("lj", idx) == idx) {
+                count++;
+                idx = idx + 2;
+            } else if (word.indexOf("nj", idx) == idx) {
+                count++;
+                idx = idx + 2;
+            } else if (word.indexOf("s=", idx) == idx) {
+                count++;
+                idx = idx + 2;
+            } else if (word.indexOf("z=", idx) == idx) {
+                count++;
+                idx = idx + 2;
+            } else {
+                count++;
+                idx++;
+            }
+        }
+        return count;
+    }
+}
