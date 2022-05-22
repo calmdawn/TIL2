a2babad98d0494b1a5f7a4c25707183f35acac70
calmdawn <qnfqus@gmail.com>
2022-05-23 02:57:59 +0900

    기본수학1 No2 ~ No5 complete

---
+++ 코테준비/src/백준/기본수학1/No5_10250.java
@@ -0,0 +1,16 @@
+package 백준.기본수학1;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class No5_10250 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        bw.flush();
+        bw.close();
+    }
+}
