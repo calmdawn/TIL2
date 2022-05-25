9a0ce96bdcf9c222c3926221374605daefebf315
calmdawn <qnfqus@gmail.com>
2022-05-26 01:39:52 +0900

    재귀 no2 ~ no3 complete

---
+++ 코테준비/src/백준/재귀/No2_10870.java
@@ -0,0 +1,26 @@
+package 백준.재귀;
+
+import java.io.*;
+
+public class No2_10870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(fibonacci(n)));
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static int fibonacci(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return fibonacci(n - 2) + fibonacci(n - 1);
+    }
+}
