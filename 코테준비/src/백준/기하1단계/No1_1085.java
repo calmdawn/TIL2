d1b9efb24f29fbc62a68a603806f5eb0a0289674
CalmDawn <qnfqus@gmail.com>
2022-06-06 00:10:03 +0900

    백준 기하1단계 시작, No1 complete

---
+++ 코테준비/src/백준/기하1단계/No1_1085.java
@@ -0,0 +1,27 @@
+package 백준.기하1단계;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class No1_1085 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int min = 1000;
+        min = Math.min(min, x);
+        min = Math.min(min, y);
+        min = Math.min(min, w-x);
+        min = Math.min(min, h-y);
+
+        bw.write(String.valueOf(min));
+        bw.flush();
+        bw.close();
+    }
+}
