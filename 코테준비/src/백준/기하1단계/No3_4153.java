3605129294eb4ce61576acaf53c9c8aea6ea5c2f
CalmDawn <qnfqus@gmail.com>
2022-06-07 16:12:45 +0900

    기하1단계 No2 ~ No3 complete

---
+++ 코테준비/src/백준/기하1단계/No3_4153.java
@@ -0,0 +1,40 @@
+package 백준.기하1단계;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class No3_4153 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0 && c == 0)
+                break;
+
+            if (a <= b && b <= c)
+                bw.write(checkCase(a, b, c) + "\n");
+            else if (a <= c && c <= b)
+                bw.write(checkCase(a, c, b) + "\n");
+            else
+                bw.write(checkCase(b, c, a) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static String checkCase(int a, int b, int c) {
+        if (Math.pow(c, 2) == Math.pow(a, 2) + Math.pow(b, 2))
+            return "right";
+        else
+            return "wrong";
+    }
+}
---
+++ 코테준비/src/백준/기하1단계/No4_2477.java
@@ -0,0 +1,20 @@
+package 백준.기하1단계;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class No4_2477 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int standardNum = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+
+
+        bw.flush();
+        bw.close();
+    }
+
+}
