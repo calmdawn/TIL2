14e51df5e380e2df2998c28b572046dbe47e947e
CalmDawn <qnfqus@gmail.com>
2022-06-17 21:33:30 +0900

    백준 스택 start !

---
+++ 코테준비/src/백준/Lv19_스택/No1_10828.java
@@ -0,0 +1,14 @@
+package 백준.Lv19_스택;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class No1_10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.flush();
+        bw.close();
+    }
+}
