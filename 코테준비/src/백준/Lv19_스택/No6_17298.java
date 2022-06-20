731510d75232a33cf0ab33dec48a623f4f8c59ed
calmdawn <qnfqus@gmail.com>
2022-06-20 21:40:59 +0900

    스택 No6 fail

---
+++ 코테준비/src/백준/Lv19_스택/No6_17298.java
@@ -0,0 +1,16 @@
+package 백준.Lv19_스택;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class No6_17298 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        bw.flush();
+        bw.close();
+    }
+}
