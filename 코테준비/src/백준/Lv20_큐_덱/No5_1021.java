fb2b66767793a807e0e2aae72181bfb12913f76f
calmdawn <qnfqus@gmail.com>
2022-06-23 23:30:26 +0900

    덱 no5 fail

---
+++ 코테준비/src/백준/Lv20_큐_덱/No5_1021.java
@@ -0,0 +1,36 @@
+package 백준.Lv20_큐_덱;
+
+import java.io.*;
+import java.util.*;
+
+public class No5_1021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Deque<Integer> deque = new LinkedList<>();
+        Deque<Integer> tempDeque = new LinkedList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] locate = new int[M];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <=N ; i++)
+            deque.add(i);
+        tempDeque = deque;
+        tempDeque.pop();
+
+        System.out.println(deque.peek());
+        System.out.println(tempDeque.peek());
+        for (int i = 0; i < M; i++)
+            locate[i] = Integer.parseInt(st.nextToken());
+
+
+
+
+        bw.flush();
+        bw.close();
+    }
+}
