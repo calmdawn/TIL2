e174481bde9575fd6499aae8fd9d53958092e49e
CalmDawn <qnfqus@gmail.com>
2022-06-26 20:41:11 +0900

    분할정복 No6 complete

--- 코테준비/src/백준/Lv21_분할정복/No6_2740.java
+++ 코테준비/src/백준/Lv21_분할정복/No6_2740.java
@@ -1,20 +1,23 @@
 package 백준.Lv21_분할정복;
 
 import java.io.*;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
-public class No_2740 {
+public class No6_2740 {
 
-    private static  StringBuilder sb = new StringBuilder();
+    private static StringBuilder sb = new StringBuilder();
+    private static int N;
+    private static int M;
+    private static int K;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         int[][] A = new int[N][M];
 
         for (int i = 0; i < N; i++) {
@@ -25,7 +28,7 @@ public class No_2740 {
 
         st = new StringTokenizer(br.readLine());
         M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
         int[][] B = new int[M][K];
 
         for (int i = 0; i < M; i++) {
@@ -34,30 +37,25 @@ public class No_2740 {
                 B[i][j] = Integer.parseInt(st.nextToken());
         }
 
-        getResult(A, B, M);
+        getResult(A, B);
 
         bw.write(sb.toString().trim());
         bw.flush();
         bw.close();
     }
 
-    //
-//    1 2   -1 -2  0
-//    3 4    0  0  3
-//    5 6
-//
-    private static void getResult(int[][] A, int[][] B,int M) {
-        for (int i = 0; i < A.length; i++) {
-            getLine(A[i], B, M);
+    private static void getResult(int[][] A, int[][] B) {
+        for (int i = 0; i < N; i++) {
+            getLine(A[i], B);
             sb.append("\n");
         }
     }
 
-    private static void getLine(int[] aLine, int[][] b,int M) {
-        for (int i = 0; i < M; i++) {
+    private static void getLine(int[] aLine, int[][] b) {
+        for (int i = 0; i < K; i++) {
             int sum = 0;
-            for (int j = 0; j < b.length; j++) {
-                sum = aLine[j] * b[i][j];
+            for (int j = 0; j < M; j++) {
+                sum = sum + aLine[j] * b[j][i];
             }
             sb.append(sum).append(" ");
         }
