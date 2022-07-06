4be4f449391a8c7d20967e88d3e975ab897549af
calmdawn <qnfqus@gmail.com>
2022-07-06 23:56:17 +0900

    분할정복 No8 fail.

---
+++ 코테준비/src/백준/Lv21_분할정복/No8_11444.java
@@ -0,0 +1,23 @@
+package 백준.Lv21_분할정복;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class No8_11444 {
+    private static long[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = Long.parseLong(br.readLine());
+
+        if (n == 1)
+
+
+            bw.flush();
+        bw.close();
+    }
+
+
+}
