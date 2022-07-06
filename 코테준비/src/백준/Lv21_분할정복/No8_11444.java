b2b15b20fa4f416007c678550175cc890c08f4ec
calmdawn <qnfqus@gmail.com>
2022-07-07 00:11:49 +0900

    분할정복 No8 fail. timeOut

--- 코테준비/src/백준/Lv21_분할정복/No8_11444.java
+++ 코테준비/src/백준/Lv21_분할정복/No8_11444.java
@@ -4,18 +4,22 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class No8_11444 {
-    private static long[] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         long n = Long.parseLong(br.readLine());
+        long n0 = 0;
+        long n1 = 1;
+        long n2 = 0;
 
-        if (n == 1)
-            bw.flush();
+        for (long i = 2; i <= n; i++) {
+            n2 = (n1 + n0) % 1000000007;
+            n0 = n1 % 1000000007;
+            n1 = n2 % 1000000007;
+        }
+
+        bw.write(String.valueOf(n2));
         bw.close();
     }
-
-
 }
