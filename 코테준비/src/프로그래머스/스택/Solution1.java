ca5d6888173e60fd873e4ac106895f387d2134db
CalmDawn <qnfqus@gmail.com>
2022-07-18 01:05:41 +0900

    프로그래머스 스택 No1 complete

---
+++ 코테준비/src/프로그래머스/스택/Solution1.java
@@ -0,0 +1,41 @@
+package 프로그래머스.스택;
+
+import java.util.Stack;
+//Lv2 기능개발
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer;
+        int day = 0;
+        Stack<Integer> dayStack = new Stack<>();
+        Stack<Integer> resultStack = new Stack<>();
+
+        for(int i=0; i<progresses.length; i++){
+            int progress = progresses[i] + speeds[i] * day;
+            while(progress < 100){
+                progress = progress + speeds[i];
+                day++;
+            }
+            dayStack.push(day);
+        }
+
+        int num = dayStack.pop();
+        int cnt = 1;
+        while(!dayStack.isEmpty()){
+            if(num == dayStack.peek()){
+                cnt++;
+                dayStack.pop();
+            }else{
+                resultStack.push(cnt);
+                num = dayStack.pop();
+                cnt = 1;
+            }
+        }
+        resultStack.push(cnt);
+
+        answer = new int[resultStack.size()];
+        for(int i=0; i<answer.length; i++)
+            answer[i] = resultStack.pop();
+
+        return answer;
+    }
+}
