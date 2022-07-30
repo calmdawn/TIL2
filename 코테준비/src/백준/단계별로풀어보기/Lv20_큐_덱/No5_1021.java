d57908a91ddcf31361837fc002b92b76e68c451f
calmdawn <qnfqus@gmail.com>
2022-07-31 00:22:59 +0900

    백준 큐_덱  No5 complete

--- 코테준비/src/백준/단계별로풀어보기/Lv20_큐_덱/No5_1021.java
+++ 코테준비/src/백준/단계별로풀어보기/Lv20_큐_덱/No5_1021.java
@@ -9,27 +9,44 @@ public class No5_1021 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         Deque<Integer> deque = new LinkedList<>();
-        Deque<Integer> tempDeque = new LinkedList<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int[] locate = new int[M];
         st = new StringTokenizer(br.readLine());
-
-        for (int i = 1; i <=N ; i++)
-            deque.add(i);
-        tempDeque = deque;
-        tempDeque.pop();
-
-        System.out.println(deque.peek());
-        System.out.println(tempDeque.peek());
         for (int i = 0; i < M; i++)
             locate[i] = Integer.parseInt(st.nextToken());
 
+        for (int i = 1; i <= N; i++)
+            deque.offer(i);
 
+        int mov = 0;
+        int result = 0;
+        for (int idx = 0; idx < M; idx++) {
 
+            while (deque.peek() != locate[idx]) {
+                mov++;
+                deque.offerLast(deque.pollFirst());
+            }
 
+            if (mov > deque.size() / 2) {
+                while (mov != 0) {    //원위치로
+                    deque.offerFirst(deque.pollLast());
+                    mov--;
+                }
+
+                while (deque.peek() != locate[idx]) {
+                    mov++;
+                    deque.offerFirst(deque.pollLast());
+                }
+            }
+            result = result + mov;
+            mov = 0;
+            deque.poll();
+        }
+
+        bw.write(String.valueOf(result));
         bw.flush();
         bw.close();
     }
