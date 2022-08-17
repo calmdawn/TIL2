6fe6314347010d606e6a1589a68198916ac9708a
calmdawn <qnfqus@gmail.com>
2022-08-18 07:14:10 +0900

    백준 기타 No2743 complete

---
+++ 코테준비/src/백준/백준기타/BaseClass.java
@@ -0,0 +1,13 @@
+package 백준.백준기타;
+
+import java.io.*;
+
+public class BaseClass {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.flush();
+        bw.close();
+    }
+}
---
+++ 코테준비/src/백준/백준기타/No2743.java
@@ -0,0 +1,13 @@
+package 백준.백준기타;
+
+import java.io.*;
+
+public class No2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(br.readLine().length()));
+        bw.flush();
+        bw.close();
+    }
+}
