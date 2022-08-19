a49f7a0f076cf949df367ee73e29daa875ad0f67
calmdawn <qnfqus@gmail.com>
2022-08-19 18:22:55 +0900

    백준 기타 No11005, No2745 name refactoring

--- 코테준비/src/백준/백준기타/No2745.java
+++ 코테준비/src/백준/백준기타/No2745.java
@@ -3,26 +3,29 @@ package 백준.백준기타;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class No11005 {
+public class No2745 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+        String N = st.nextToken();
         int B = Integer.parseInt(st.nextToken());
-        String result = getResult(N, B);
-        bw.write(result);
+        int result = getResult(N, B);
+        bw.write(String.valueOf(result));
         bw.flush();
         bw.close();
     }
 
-    private static String getResult(int N, int B) {
-        StringBuffer sb = new StringBuffer();
-        String base = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        while (N != 0) {
-            sb.append(base.charAt(N % B));
-            N = N / B;
+    private static int getResult(String N, int B) {
+        int num = 0;
+        for (int i = 0; i < N.length(); i++) {
+            char ch = N.charAt(i);
+            if ('0' <= ch && ch <= '9') {
+                num = num * B + (ch - '0');
+            } else {
+                num = num * B + (ch - 'A' + 10);
+            }
         }
-        return sb.reverse().toString();
+        return num;
     }
 }
