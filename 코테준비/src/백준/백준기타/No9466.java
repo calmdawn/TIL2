38c727eb5d2a6729515d83d86487813249e421db
calmdawn <qnfqus@gmail.com>
2022-08-24 23:47:28 +0900

    백준 기타 No9466 complete

--- 코테준비/src/백준/백준기타/No9466.java
+++ 코테준비/src/백준/백준기타/No9466.java
@@ -1,10 +1,13 @@
 package 백준.백준기타;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class No9466 {
     private static int teamCnt;
+    private static List<Integer> groupList;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,20 +21,29 @@ public class No9466 {
             for (int i = 1; i <= n; i++)  //i번째 학생이 원하는 팀원
                 students[i][0] = Integer.parseInt(st.nextToken());
             boolean[] isVisited = new boolean[n + 1];
-            teamCnt = 0;
+            teamCnt = n;
+            groupList = new ArrayList<>();
             for (int i = 1; i <= n; i++) {
                 if (isVisited[i] == false)
-                    matchingTeam(i, students[i][0], isVisited, students);
+                    matchingTeam(i, isVisited, students);
             }
             bw.write(teamCnt + "\n");
         }
         bw.flush();
         bw.close();
     }
-    //처음 팀선택을 시작한 학생 == 현재 선택할 학생인경우 teamCnt++
-    private static void matchingTeam(int prev, int current, boolean[] isVisited, int[][] students) {
 
+    private static void matchingTeam(int current, boolean[] isVisited, int[][] students) {
+        isVisited[current] = true;
+        groupList.add(current);
+        int next = students[current][0];
+        if (isVisited[next]) {    //다음에 선택할 학생이 이미 선택끝난 경우
+            int locate = groupList.indexOf(next);
+            if (locate >= 0)
+                teamCnt = teamCnt - (groupList.size() - locate);
+            groupList.clear();
+        } else {
+            matchingTeam(next, isVisited, students);
+        }
     }
-
-
 }
