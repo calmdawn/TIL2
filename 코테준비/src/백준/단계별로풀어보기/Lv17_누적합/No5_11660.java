a7778505755a2576d35a8a2ff9e1655480591814
calmdawn <qnfqus@gmail.com>
2022-09-01 02:12:19 +0900

    백준 누적합 No5 complete

---
+++ 코테준비/src/백준/단계별로풀어보기/Lv17_누적합/No5_11660.java
@@ -0,0 +1,37 @@
+package 백준.단계별로풀어보기.Lv17_누적합;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class No5_11660 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] board = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken()) + board[i][j - 1];
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int sum = 0;
+            for (int j = x1; j <= x2; j++) {
+                sum = sum + board[j][y2] - board[j][y1 - 1];
+            }
+            bw.write(sum + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
